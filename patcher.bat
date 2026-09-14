f8c130825f9f2e87988bcf5e67645f5860f91075
peuloom <50783223+nectaroads@users.noreply.github.com>
2026-09-13 23:26:30 -0300

    up

--- patcher.bat
+++ patcher.bat
@@ -1,21 +1,25 @@
 @echo off
-setlocal
+setlocal enableextensions
+cd /d "%~dp0"
+
 set "WINDOW_NAME=%~1"
 
 if not "%WINDOW_NAME%"=="" (
     taskkill /FI "WINDOWTITLE eq %WINDOW_NAME%*" /F /T >nul 2>&1
 )
 
-timeout /t 2 /nobreak >nul
+timeout /t 3 /nobreak >nul
 
-if exist "Daydream.exe.new" (
-    move /y "Daydream.exe.new" "Daydream.exe" >nul
+if exist "Daydream.exe.tmp" (
+    del /f /q "Daydream.exe" >nul 2>&1
+    move /y "Daydream.exe.tmp" "Daydream.exe"
 )
 
-if exist "Daydream.pck.new" (
-    move /y "Daydream.pck.new" "Daydream.pck" >nul
+if exist "Daydream.pck.tmp" (
+    del /f /q "Daydream.pck" >nul 2>&1
+    move /y "Daydream.pck.tmp" "Daydream.pck"
 )
 
-start "" "Daydream.exe"
+start "" "%~dp0Daydream.exe"
 
 exit
